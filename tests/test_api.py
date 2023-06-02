10911290dbcb2d206e0288d0f13a0576343e42d4
Marcelo Leivas <catskilletmarcelo@luizalabs.com>
2023-06-02 10:54:08 -0300

    add tests to token and refresh token routes

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,45 @@
+import json
+
 import pytest
 
 from pamps.models import Social, Like
 
 
 @pytest.mark.order(1)
+def test_try_to_login_with_invalid_credentials_and_get_401(api_client):
+    response = api_client.post(
+        "/token",
+        data={"username": "non-existent-user", "password": "invalid-password"},
+        headers={"Content-Type": "application/x-www-form-urlencoded"},
+    )
+    result = response.json()
+
+    assert response.status_code == 401
+    assert result["detail"] == "Incorrect username or password"
+
+
+@pytest.mark.order(2)
+def test_get_successfully_user_refresh_token(api_client, api_client_user_1):
+    refresh_token = api_client.post(
+        "/token",
+        data={"username": "user_1", "password": "user_1"},
+        headers={"Content-Type": "application/x-www-form-urlencoded"},
+    ).json()["refresh_token"]
+
+    response = api_client.post(
+        "/refresh_token",
+        content=json.dumps({"refresh_token": refresh_token}),
+        headers={"Content-Type": "application/json"},
+    )
+    result = response.json()
+
+    assert response.status_code == 200
+    assert result.get("access_token")
+    assert result.get("refresh_token")
+    assert result.get("token_type")
+
+
+@pytest.mark.order(3)
 def test_post_create_user_1(api_client_user_1):
     """Create 2 posts with user 1"""
     for n in (1, 2):
@@ -19,7 +55,7 @@ def test_post_create_user_1(api_client_user_1):
         assert result["parent_id"] is None
 
 
-@pytest.mark.order(2)
+@pytest.mark.order(4)
 def test_reply_on_post_1(api_client, api_client_user_1, api_client_user_2):
     """each user will add a reply to the first post"""
     posts = api_client.get("/post/user/user_1/").json()
@@ -38,7 +74,7 @@ def test_reply_on_post_1(api_client, api_client_user_1, api_client_user_2):
         assert result["parent_id"] == first_post["id"]
 
 
-@pytest.mark.order(3)
+@pytest.mark.order(5)
 def test_post_list_without_replies(api_client):
     response = api_client.get("/post/")
     assert response.status_code == 200
@@ -49,7 +85,7 @@ def test_post_list_without_replies(api_client):
         assert "hello test" in result["text"]
 
 
-@pytest.mark.order(4)
+@pytest.mark.order(6)
 def test_post_1_detail(api_client):
     posts = api_client.get("/post/user/user_1/").json()
     first_post = posts[0]
@@ -69,7 +105,7 @@ def test_post_1_detail(api_client):
         assert "reply from user" in reply["text"]
 
 
-@pytest.mark.order(5)
+@pytest.mark.order(7)
 def test_all_posts_from_user_1(api_client):
     response = api_client.get("/post/user/user_1/")
     assert response.status_code == 200
@@ -80,7 +116,7 @@ def test_all_posts_from_user_1(api_client):
         assert "hello test" in result["text"]
 
 
-@pytest.mark.order(6)
+@pytest.mark.order(8)
 def test_all_posts_from_user_1_with_replies(api_client):
     response = api_client.get(
         "/post/user/user_1/", params={"include_replies": True}
@@ -90,7 +126,7 @@ def test_all_posts_from_user_1_with_replies(api_client):
     assert len(results) == 3
 
 
-@pytest.mark.order(7)
+@pytest.mark.order(9)
 def test_user_1_follows_user_2(session, api_client_user_1, api_client_user_2):
     response = api_client_user_1.post("/user/follow/2")
     assert response.status_code == 204
@@ -104,7 +140,7 @@ def test_user_1_follows_user_2(session, api_client_user_1, api_client_user_2):
     assert following_succeeded
 
 
-@pytest.mark.order(8)
+@pytest.mark.order(10)
 def test_user_tries_following_non_existent_user_and_get_404(
         session, api_client_user_1, api_client_user_2
 ):
@@ -120,7 +156,7 @@ def test_user_tries_following_non_existent_user_and_get_404(
     assert not following_succeeded
 
 
-@pytest.mark.order(9)
+@pytest.mark.order(11)
 def test_user_tries_following_negative_integer_and_get_400(session, api_client_user_1):
     response = api_client_user_1.post("/user/follow/-1")
     assert response.status_code == 400
@@ -128,7 +164,7 @@ def test_user_tries_following_negative_integer_and_get_400(session, api_client_u
     assert result["detail"] == "Invalid user_id"
 
 
-@pytest.mark.order(10)
+@pytest.mark.order(12)
 def test_user_tries_following_himself_and_get_400(session, api_client_user_1):
     response = api_client_user_1.post("/user/follow/1")
     assert response.status_code == 400
@@ -136,7 +172,7 @@ def test_user_tries_following_himself_and_get_400(session, api_client_user_1):
     assert result["detail"] == "Invalid user_id"
 
 
-@pytest.mark.order(11)
+@pytest.mark.order(13)
 def test_user_tries_following_again_and_get_204(
         session, api_client_user_1, api_client_user_2
 ):
@@ -151,7 +187,7 @@ def test_user_tries_following_again_and_get_204(
     assert following_count == 1
 
 
-@pytest.mark.order(12)
+@pytest.mark.order(14)
 def test_user_tries_unfollowing_negative_integer_and_get_400(
         session, api_client_user_1
 ):
@@ -161,7 +197,7 @@ def test_user_tries_unfollowing_negative_integer_and_get_400(
     assert result["detail"] == "Invalid user_id"
 
 
-@pytest.mark.order(13)
+@pytest.mark.order(15)
 def test_user_tries_unfollowing_himself_and_get_400(session, api_client_user_1):
     response = api_client_user_1.delete("/user/follow/1")
     assert response.status_code == 400
@@ -169,7 +205,7 @@ def test_user_tries_unfollowing_himself_and_get_400(session, api_client_user_1):
     assert result["detail"] == "Invalid user_id"
 
 
-@pytest.mark.order(14)
+@pytest.mark.order(16)
 def test_user_tries_unfollowing_non_existent_user_and_get_404(
         session, api_client_user_1, api_client_user_2
 ):
@@ -185,7 +221,7 @@ def test_user_tries_unfollowing_non_existent_user_and_get_404(
     assert not following_succeeded
 
 
-@pytest.mark.order(15)
+@pytest.mark.order(17)
 def test_user_unfollows_user_and_get_204(
         session, api_client_user_1, api_client_user_2
 ):
@@ -200,7 +236,7 @@ def test_user_unfollows_user_and_get_204(
     assert following_count == 0
 
 
-@pytest.mark.order(16)
+@pytest.mark.order(18)
 def test_user_tries_unfollowing_not_following_and_get_204(
         session, api_client_user_1, api_client_user_2
 ):
@@ -215,7 +251,7 @@ def test_user_tries_unfollowing_not_following_and_get_204(
     assert following_count == 0
 
 
-@pytest.mark.order(17)
+@pytest.mark.order(19)
 def test_user_likes_his_own_post(session, api_client_user_1):
     response = api_client_user_1.post("/post/1/like")
     like_is_on_db = (
@@ -232,7 +268,7 @@ def test_user_likes_his_own_post(session, api_client_user_1):
     assert like_is_on_db
 
 
-@pytest.mark.order(18)
+@pytest.mark.order(20)
 def test_user_likes_a_post(session, api_client_user_2):
     response = api_client_user_2.post("/post/1/like")
     like_is_on_db = (
@@ -249,7 +285,7 @@ def test_user_likes_a_post(session, api_client_user_2):
     assert like_is_on_db
 
 
-@pytest.mark.order(19)
+@pytest.mark.order(21)
 def test_try_to_like_non_existent_post_and_get_404(session, api_client_user_2):
     response = api_client_user_2.post("/post/12/like")
     result = response.json()
@@ -258,7 +294,7 @@ def test_try_to_like_non_existent_post_and_get_404(session, api_client_user_2):
     assert result["detail"] == "Post not found"
 
 
-@pytest.mark.order(20)
+@pytest.mark.order(22)
 def test_user_dislikes_a_post(session, api_client_user_2):
     response = api_client_user_2.delete("/post/1/like")
     like_is_on_db = (
@@ -275,7 +311,7 @@ def test_user_dislikes_a_post(session, api_client_user_2):
     assert not like_is_on_db
 
 
-@pytest.mark.order(21)
+@pytest.mark.order(23)
 def test_try_to_dislike_non_existent_post_and_get_404(session, api_client_user_2):
     response = api_client_user_2.delete("/post/12/like")
     result = response.json()
@@ -284,7 +320,7 @@ def test_try_to_dislike_non_existent_post_and_get_404(session, api_client_user_2
     assert result["detail"] == "Post not found"
 
 
-@pytest.mark.order(22)
+@pytest.mark.order(24)
 def test_try_to_dislike_non_existent_liked_post_and_get_404(session, api_client_user_2):
     response = api_client_user_2.delete("/post/2/like")
     result = response.json()
